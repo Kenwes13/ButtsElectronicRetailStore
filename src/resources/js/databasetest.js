0719d712ccedfd7eb66419023df7fcd81c6d2c27
James <james101994@yahoo.com>
2016-10-18 18:05:07 -0700

    semi-working router

--- src/resources/js/databasetest.js
+++ src/resources/js/databasetest.js
@@ -3,19 +3,19 @@
  */
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'test',
-    database : 'mydb'
+  host     : 'localhost',
+  user     : 'root',
+  password : '8552312j',
+  database : 'test'
 });
 
 connection.connect();
 
-connection.query('SELECT * from Customer', function(err, rows, fields) {
-    if (!err)
-        console.log('The solution is: ', rows);
-    else
-        console.log('Error while performing Query.');
+connection.query('SELECT * from test.Login', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
 });
 
 connection.end();
