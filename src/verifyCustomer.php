8b3cb80b277b642516c64f0ceac01ddb725d3250
bridgegade <avyu94@gmail.com>
2016-10-27 20:23:55 -0700

    login and sign up features added, need to add employee log in and logging out

---
+++ src/verifyCustomer.php
@@ -0,0 +1,26 @@
+<?php
+include "connectdb.php";
+
+$data = json_decode(file_get_contents("php://input"));
+
+$username = mysqli_real_escape_string($conn,$data->uName);
+$password = mysqli_real_escape_string($conn,$data->pWord);
+
+
+
+$query= "SELECT * FROM Customer WHERE CustomerName ='".$username."'";
+$result = mysqli_query($conn, $query);
+
+$row=mysqli_fetch_array($result);
+if(empty($row)){
+
+echo "Username or password incorrect";
+}
+else{
+	echo "success";
+	$_SESSION['currentCustomer'] = $username;
+}
+
+
+
+?>
