aa1695817a12dd6893c80c76ee4595bf1f17da4f
jaye1 <james101994@yahoo.com>
2016-10-28 13:35:45 -0700

    Needed to query product info later

---
+++ src/productpage.php
@@ -0,0 +1,14 @@
+<?php
+include "connectdb.php";
+$data = json_decode(file_get_contents("php://input"));
+
+$productName = mysqli_real_escape_string($conn,$data->name);
+
+$query= "SELECT * FROM Product WHERE ProductName = '$productName'";
+$result = mysqli_query($conn, $query);
+
+while($row=mysqli_fetch_array($result)){
+	$data[] = $row;
+}
+echo json_encode($data);
+?>
