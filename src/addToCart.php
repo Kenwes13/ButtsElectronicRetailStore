031fca59cab4e92b5492d0709b7614b38309bdac
James <james101994@yahoo.com>
2016-11-03 22:44:25 -0700

    ITS BACK

---
+++ src/addToCart.php
@@ -0,0 +1,13 @@
+<?php
+include "connectdb.php";
+$data = json_decode(file_get_contents("php://input"));
+
+$pName = mysqli_real_escape_string($conn,$data->pName);
+$quantity = mysqli_real_escape_string($conn,$data->quantity);
+//$query = $_POST['query'];
+
+$query = "INSERT INTO Cart(ProductName, Quantity) VALUES ('".$pName."', '".$quantity."')";
+$result = mysqli_query($conn, $query);
+
+echo($result);
+?>
