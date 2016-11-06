e323197cb0a2231e00e3b07da365f6ab46f82f92
Justin <zhentingwan@gmail.com>
2016-11-05 18:22:31 -0700

    optimize

--- src/model/search.php
+++ src/model/search.php
@@ -2,17 +2,24 @@
 include "connectdb.php";
 $input = json_decode(file_get_contents("php://input"));
 
+if(isset($input->name)){
 $name = mysqli_real_escape_string($conn,$input->name);
-
+}
 if(isset($input->category)){
 $category = mysqli_real_escape_string($conn,$input->category);
-$query= "SELECT * FROM Product WHERE Category='".$category."' AND ProductName LIKE '%".$name."%' LIMIT 20";
 }
-else{
+if(!empty($name) && empty($category)){
 $query= "SELECT * FROM Product WHERE ProductName LIKE '%".$name."%' LIMIT 20";
 }
-
-
+else if(empty($name) && !empty($category)){
+$query= "SELECT * FROM Product WHERE Category='".$category."' LIMIT 20";
+}
+else if(empty($name) && empty($category)){
+$query= "SELECT * FROM Product ORDER BY CreatedAt LIMIT 20 ";
+}
+else{
+$query= "SELECT * FROM Product WHERE Category='".$category."' AND ProductName LIKE '%".$name."%' LIMIT 20";
+}
 $result = mysqli_query($conn, $query);
 
 while($row=mysqli_fetch_array($result)){
