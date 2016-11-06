ee39e6f588eeccf8029c7897b561c2542061c0ee
Justin <zhentingwan@gmail.com>
2016-11-05 17:46:12 -0700

    fixed search function.

--- src/model/search.php
+++ src/model/search.php
@@ -3,11 +3,16 @@ include "connectdb.php";
 $input = json_decode(file_get_contents("php://input"));
 
 $name = mysqli_real_escape_string($conn,$input->name);
+
+if(isset($input->category)){
 $category = mysqli_real_escape_string($conn,$input->category);
-
-
 $query= "SELECT * FROM Product WHERE Category='".$category."' AND ProductName LIKE '%".$name."%' LIMIT 20";
-//$query= "SELECT * FROM Product WHERE ProductName='".$name."' LIMIT 20";
+}
+else{
+$query= "SELECT * FROM Product WHERE ProductName LIKE '%".$name."%' LIMIT 20";
+}
+
+
 $result = mysqli_query($conn, $query);
 
 while($row=mysqli_fetch_array($result)){
