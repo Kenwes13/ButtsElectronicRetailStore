caf5f7e824acfc042b99b39e3bc4f33f547b7331
bridgegade <avyu94@gmail.com>
2016-11-18 19:45:55 -0800

    uncommented James's get orders by customer logic

--- src/model/getorders.php
+++ src/model/getorders.php
@@ -4,10 +4,10 @@ $data = json_decode(file_get_contents("php://input"));
 
 $cName = mysqli_real_escape_string($conn,$data->cName);
 
-/*$query= "SELECT * FROM Customer WHERE CustomerName = '".$cName."'";
+$query= "SELECT * FROM Customer WHERE CustomerName = '".$cName."'";
 $result = mysqli_query($conn, $query);
-$row = mysqli_fetch_array($result ,MYSQLI_ASSOC);*/
-$cID = 1;//$row["Customerid"];
+$row = mysqli_fetch_array($result ,MYSQLI_ASSOC);
+$cID = $row["Customerid"];
 
 $query = "SELECT * FROM orderhistory, Product WHERE orderhistory.ProductName = Product.ProductName AND orderhistory.Customerid = ".$cID."";
 //$query = "SELECT * FROM orderhistory";
